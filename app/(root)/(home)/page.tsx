ee67261fd761157be19098dc45e3088cf202b2fb
arvindpndit <arvindpandit9808@gmail.com>
2023-11-03 22:16:15 +0530

    feat: add auth using clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { ClerkProvider } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>
+    <ClerkProvider>
+      <UserButton />
       <div className="h1-bold">stack overflow</div>
       <div className="h2-bold">testing css</div>
-    </>
+    </ClerkProvider>
   );
 }
