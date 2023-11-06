c6641939492c1642d5e492cd9ef534d0edbb8585
arvindpndit <arvindpandit9808@gmail.com>
2023-11-06 22:00:21 +0530

    feat: add basic navbar layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import { ClerkProvider } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <ClerkProvider>
-      <UserButton />
-      <div className="h1-bold">stack overflow</div>
-      <div className="h2-bold">testing css</div>
+      <Navbar />
     </ClerkProvider>
   );
 }
