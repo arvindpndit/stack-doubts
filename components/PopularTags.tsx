0fa69d99cf65b7c26eb0cf93425503c66b90c273
arvindpndit <arvindpandit9808@gmail.com>
2023-11-07 11:31:32 +0530

    feat: add static tags in right sidebar

--- components/PopularTags.tsx
+++ components/PopularTags.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const PopularTags = () => {
-  return <div>PopularTags</div>;
+  const popularTags: Array<string> = ["React", "Next", "CSS", "Redux", "Node"];
+  return (
+    <div>
+      <h1 className="font-bold text-xl mt-5">Popular Tags</h1>
+      {popularTags.map((tag) => {
+        return (
+          <div className="my-5 text-xs p-2 bg-blue-300 w-16 rounded-lg text-blue-800 shadow-md">
+            {tag}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PopularTags;
