4a110f08f153a794fe6d5250fcbf5d2c02fbed86
arvindpndit <arvindpandit9808@gmail.com>
2023-11-07 11:48:54 +0530

    style: add styling to search bar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,16 +9,18 @@ import RightSideBar from "@/components/RightSideBar";
 export default function Home() {
   return (
     <ClerkProvider>
-      <Navbar />
-      <div className="flex justify-between px-5">
-        <div className="w-1/5 border ">
-          <LeftSideBar />
-        </div>
-        <div className="w-full px-4">
-          <MainContainer />
-        </div>
-        <div className="w-2/6 border">
-          <RightSideBar />
+      <div className="bg-white">
+        <Navbar />
+        <div className="flex justify-between px-5">
+          <div className="w-1/5 border ">
+            <LeftSideBar />
+          </div>
+          <div className="w-full px-4">
+            <MainContainer />
+          </div>
+          <div className="w-2/6 border">
+            <RightSideBar />
+          </div>
         </div>
       </div>
     </ClerkProvider>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
+import searchIcon from "../public/assets/icons/search.svg";
 
 import devLogo from "../public/assets/images/logo-light.svg";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="h-20  flex flex-row justify-between items-center px-12">
-      <Image src={devLogo} alt="devLogo" />
-      <div>Search bar</div>
+    <div className="h-20 flex flex-row justify-between items-center px-12">
+      <Image src={devLogo} alt="devLogo" height={28} />
+      <div className="relative flex items-center">
+        <input
+          type="text"
+          placeholder="Search anything globally..."
+          className="w-96 h-12 px-4 py-2 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none"
+        />
+        <Image
+          src={searchIcon}
+          alt="Search Icon"
+          width={20}
+          height={20}
+          className="absolute right-2 top-3"
+        />
+      </div>
       <UserButton />
     </div>
   );
