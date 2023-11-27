d365c1e9a97acd73323dff317967129fa29a3651
arvindpndit <arvindpandit9808@gmail.com>
2023-11-27 21:52:56 +0530

    feat: create question model

---
+++ database/question-model.ts
@@ -0,0 +1,32 @@
+import { Schema, model, Document, Model } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+interface IQuestion extends Document {
+  title: string;
+  content: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId[];
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const questionSchema = new Schema<IQuestion>({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  views: { type: Number, default: 0 },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+// 3. Create a Model.
+const Question: Model<IQuestion> = model("Question", questionSchema);
+
+export default Question;
