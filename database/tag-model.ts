534f8221758ba35cdf8c6b135047e48ac9be4558
arvindpndit <arvindpandit9808@gmail.com>
2023-11-27 22:09:29 +0530

    feat: create user and tag model

---
+++ database/tag-model.ts
@@ -0,0 +1,24 @@
+import { Schema, model, Document, Model } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+interface ITag extends Document {
+  name: string;
+  description: string;
+  questions: Schema.Types.ObjectId[];
+  followers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const tagSchema = new Schema<ITag>({
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: true, unique: true },
+  questions: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+// 3. Create a Model.
+const Tag: Model<ITag> = model("Tag", tagSchema);
+
+export default Tag;
---
+++ database/user-model.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Document, Model } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture: string;
+  location: string;
+  portfolioWebsite?: string;
+  reputation: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>({
+  clerkId: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String },
+  bio: { type: String },
+  picture: { type: String, required: true },
+  location: { type: String, required: true },
+  portfolioWebsite: { type: String },
+  reputation: { type: Number, default: 0 },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+});
+
+// 3. Create a Model.
+const User: Model<IUser> = model("User", userSchema);
+
+export default User;
