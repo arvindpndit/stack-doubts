afb2d1ed4c846634f5801aa0cb07927d8e48eb47
arvindpndit <arvindpandit9808@gmail.com>
2023-12-05 12:16:04 +0530

    style: change the logo of the app

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import searchIcon from "../public/assets/icons/search.svg";
 
-import devLogo from "../public/assets/images/logo-light.svg";
+import devLogo from "../public/assets/images/logo.png";
 
 const Navbar: React.FC = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bg-white h-20 flex items-center justify-between px-12">
-      <Image src={devLogo} alt="devLogo" height={28} />
+      <Image src={devLogo} alt="devLogo" height={35} />
       <div className="relative flex items-center">
         <input
           type="text"
