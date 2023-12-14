4171784e3a91b02d544b7f050478a9b2e6894451
arvindpndit <arvindpandit9808@gmail.com>
2023-12-14 22:00:58 +0530

    fix: change the type of author in question interface

--- database/question-model.ts
+++ database/question-model.ts
@@ -8,7 +8,7 @@ export interface IQuestion extends Document {
   views: number;
   upvotes: Schema.Types.ObjectId[];
   downvotes: Schema.Types.ObjectId[];
-  author: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
   answers: Schema.Types.ObjectId[];
   createdAt: Date;
 }
