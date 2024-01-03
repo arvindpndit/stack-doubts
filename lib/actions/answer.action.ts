d2de212e24c21d3c3254ae36953c4bf4b5661789
arvindpndit <arvindpandit9808@gmail.com>
2024-01-03 10:38:58 +0530

    fix: update question model after creating answer

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -3,20 +3,32 @@ import Answer from "@/database/answer-model";
 import { connectToMongoDb } from "../mongoose";
 import { Types } from "mongoose";
 import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import Question from "@/database/question-model";
+import { revalidatePath } from "next/cache";
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
     connectToMongoDb();
     const { content, author, path, question } = params;
 
-    await Answer.create({
+    const answer = await Answer.create({
       content,
       author,
       path,
       question,
     });
 
-    //revalidatePath(path);
+    await Question.findOneAndUpdate(
+      {
+        _id: question,
+      },
+      { $push: { answers: answer._id } },
+      {
+        new: true,
+      }
+    );
+
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
   }
