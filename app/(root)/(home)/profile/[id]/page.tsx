aefd5531e6a62bd77d009ef9ec3e3e3db9b9bf2d
arvindpndit <arvindpandit9808@gmail.com>
2024-01-10 20:41:33 +0530

    fix: remove direct db call from profile page

--- app/(root)/(home)/profile/[id]/page.tsx
+++ app/(root)/(home)/profile/[id]/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ProfileHeader from "@/components/partials/ProfileHeader";
 import User from "@/database/user-model";
+import QuestionCard from "@/components/partials/QuestionCard";
+import { getQuestionsByAuthorId } from "@/lib/actions/question.action";
+import { getUserById } from "@/lib/actions/user.action";
 
 const Profile = async ({ params }: { params: { id: string } }) => {
-  const userInfo = await User.findById(params.id);
+  const userInfo = await getUserById({ key: "_id", value: params.id });
 
   return (
     <div className="w-full px-1 md:px-8 mt-28 h-screen">
       <ProfileHeader userInfo={userInfo} />
+      <QuestionCard />
     </div>
   );
 };
