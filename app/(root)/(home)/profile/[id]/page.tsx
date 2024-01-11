83cd27dffbd10489d60e2321f12ca06c45dc2bb0
arvindpndit <arvindpandit9808@gmail.com>
2024-01-11 11:38:39 +0530

    refactor: move getUserById server action inside profile-header

--- app/(root)/(home)/profile/[id]/page.tsx
+++ app/(root)/(home)/profile/[id]/page.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import ProfileHeader from "@/components/partials/ProfileHeader";
-import User from "@/database/user-model";
 import QuestionCard from "@/components/partials/QuestionCard";
-import { getQuestionsByAuthorId } from "@/lib/actions/question.action";
-import { getUserById } from "@/lib/actions/user.action";
 
-const Profile = async ({ params }: { params: { id: string } }) => {
-  const userInfo = await getUserById({ key: "_id", value: params.id });
-
+const Profile = ({ params }: { params: { id: string } }) => {
   return (
     <div className="w-full px-1 md:px-8 mt-28 h-screen">
-      <ProfileHeader userInfo={userInfo} />
+      <ProfileHeader authorId={params.id} />
       <button className="font-semibold text-sm bg-green-300 p-2 rounded-md hover:bg-green-400">
         Questions Asked
       </button>
--- components/partials/ProfileHeader.tsx
+++ components/partials/ProfileHeader.tsx
@@ -3,27 +3,28 @@ import Image from "next/image";
 import BronzeMedalImg from "../../public/assets/icons/bronze-medal.svg";
 import GoldMedalImg from "../../public/assets/icons/gold-medal.svg";
 import SilverMedalImg from "../../public/assets/icons/silver-medal.svg";
+import { getUserById } from "@/lib/actions/user.action";
 
 interface ProfileHeaderProps {
-  userInfo: any;
+  authorId: string;
 }
 
-const ProfileHeader = (params: ProfileHeaderProps) => {
-  const user = params.userInfo;
+const ProfileHeader = async (params: ProfileHeaderProps) => {
+  const user = await getUserById({ key: "_id", value: params.authorId });
 
   return (
     <div className="p-4 md:p-8 lg:p-12">
       <div className="flex items-center gap-4 ">
         <img
-          src={user.picture}
+          src={user?.picture}
           width={160}
           height={160}
           alt="Picture of the author"
           className="rounded-full"
         />
         <div className="">
-          <div className="text-3xl font-bold">{user.name}</div>
-          <div className="text-gray-500">@{user.username}</div>
+          <div className="text-3xl font-bold">{user?.name}</div>
+          <div className="text-gray-500">@{user?.username}</div>
         </div>
       </div>
       <div className="mt-8">
