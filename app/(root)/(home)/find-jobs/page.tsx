87a03c482d505efacb9d937a7f1980d0d0f2d9fb
arvindpndit <arvindpandit9808@gmail.com>
2024-01-23 11:21:39 +0530

    fix: find jobs & tags link

---
+++ app/(root)/(home)/find-jobs/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full px-1 md:px-8 mt-28 h-screen">
+      <p className="text-lg font-semibold text-gray-700">
+        Find Jobs (Coming Soon!)
+      </p>
+    </div>
+  );
+};
+
+export default page;
--- app/(root)/(home)/tags/page.tsx
+++ app/(root)/(home)/tags/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Tags = () => {
-  return <div>Tags</div>;
+  return (
+    <div className="w-full px-1 md:px-8 mt-28 h-screen">
+      <p className="text-lg font-semibold text-gray-700">Tags (Coming Soon!)</p>
+    </div>
+  );
 };
 
 export default Tags;
