b5547e213bf2a5debf3499bfb3897f543624ee24
arvindpndit <arvindpandit9808@gmail.com>
2024-02-16 13:22:50 +0530

    feat: change url params with debouncing from input

--- components/common/LocalSearchBar.tsx
+++ components/common/LocalSearchBar.tsx
@@ -1,18 +1,41 @@
+"use client";
+
+import { useDebouncedCallback } from "use-debounce";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 import Image from "next/image";
 import searchIcon from "../../public/assets/icons/search.svg";
-import React from "react";
 
 interface localSearchBarProps {
   placeholder: string;
 }
 
 const LocalSearchBar = ({ placeholder }: localSearchBarProps) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    {
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set("query", term);
+      } else {
+        params.delete("query");
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }
+  }, 300);
+
   return (
     <div className="relative flex items-center mt-6">
       <input
-        type="text"
-        placeholder={placeholder}
         className="w-full h-12 px-4 py-2 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none"
+        placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <Image
         src={searchIcon}
