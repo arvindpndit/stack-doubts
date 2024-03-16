b86de9b0be694e45b42831fc9157a786a5793961
arvindpndit <arvindpandit9808@gmail.com>
2024-03-16 12:11:33 +0530

    chore: change the title of app

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 export const metadata: Metadata = {
-  title: "Stack Overflow",
+  title: "Stack Doubts",
   description:
     "A community driven platform for asking and answering programming questions",
   icons: {
