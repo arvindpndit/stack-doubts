9e03085bf6c111df3c8ccb5b855897e9d13ad406
arvindpndit <arvindpandit9808@gmail.com>
2025-03-05 12:05:41 +0530

    git commit -m "feat: add global search action"

---
+++ lib/actions/global.action.ts
@@ -0,0 +1,35 @@
+import Question from '@/database/question-model';
+import { connectToMongoDb } from '../mongoose';
+import Tag from '@/database/tag-model';
+import User from '@/database/user-model';
+
+export async function getGlobalSearchResult(searchGlobalQuery: string) {
+  try {
+    await connectToMongoDb();
+
+    const [questions, users, tags] = await Promise.all([
+      Question.find({
+        $or: [
+          { title: { $regex: searchGlobalQuery, $options: 'i' } },
+          { content: { $regex: searchGlobalQuery, $options: 'i' } },
+        ],
+      }).exec(),
+      User.find({
+        $or: [
+          { name: { $regex: searchGlobalQuery, $options: 'i' } },
+          { username: { $regex: searchGlobalQuery, $options: 'i' } },
+        ],
+      }),
+      Tag.find({
+        name: { $regex: searchGlobalQuery, $options: 'i' },
+      }),
+    ]);
+
+    //console.log(questions, users, tags);
+    return { questions, users, tags };
+  } catch (error) {
+    console.error('Error fetching :', error);
+    throw new Error('Failed to fetch');
+  }
+}
+
